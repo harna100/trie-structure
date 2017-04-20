1d9cc0cf90389ea96cada47b52be7d4cf749ae3c
Paul Harnack <harna100@mail.chapman.edu>
2017-04-19 22:32:59 -0700

    Adds basic timing and asking for a random prefix size 1-5

--- main.cpp
+++ main.cpp
@@ -2,6 +2,8 @@
 #include <map>
 #include <list>
 #include <fstream>
+#include <stdlib.h>
+#include <time.h>
 #include "TrieNode.h"
 #include "Trie.h"
 
@@ -12,22 +14,36 @@ int main(int argc, char **argv){
 		std::cout << "Enter only 1 argument (filename containing words separated by lines)\n";
 		return 1;
 	}
-	Trie trie = importWords(argv[1]);
+	clock_t t;
 
+	t = clock();
+	Trie trie = importWords(argv[1]);
+	t = clock() - t;
+	std::cout << "Took " << t << " clicks (" << ((float)t)/CLOCKS_PER_SEC << " seconds) to insert words from file\n";
 	std::string userRaw;
+	srand(time(NULL));
 	
 	while(true){
-		std::cout << "Enter prefix (-1 to quit): ";
+		int prefixLength = rand() % 5 + 1;
+		std::cout << "Enter prefix of length '" << prefixLength << "' or -1 to quit: ";
 		std::getline(std::cin, userRaw);
-		
+
 		if(userRaw == "-1"){
 			break;
 		}
+		if(userRaw.length() != prefixLength){
+			continue;
+		}
 		else{
+			std::cout << "Words found: \n"; 
+			clock_t t2 = clock();
 			std::list<std::string>* returned = trie.prefixSearch(userRaw);
+			t2 = clock() - t2;
+
 			for (std::list<std::string>::iterator i = returned->begin(); i != returned->end(); ++i){	
 		    	std::cout << *i << '\n';
 			}
+			std::cout << "Took " << t2 << " clicks (" << ((float)t2)/CLOCKS_PER_SEC << " seconds) to find words\n";
 		}
 	}
 
